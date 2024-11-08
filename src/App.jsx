fe42cb0e97222d536295ac0c8355d85909fca088
Tamaritter <tamaritter.38@gmail.com>
2024-11-08 12:52:01 +0100

    Basic

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Welcome to My Bun + React App</h1>
+        </div>
+    );
+}
+
+export default App;
