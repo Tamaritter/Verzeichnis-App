37da3eab01ccf3f253c73eec0e6c42383deaa13c
Julius Gesang <julius@familie-gesang.de>
2024-12-02 11:55:43 +0100

    Fix Links on Tutorial Page

--- src/app/[locale]/tutorials/page.tsx
+++ src/app/[locale]/tutorials/page.tsx
@@ -1,5 +1,8 @@
-import {Paper} from '@mui/material';
+'use server';
+import {List, Paper, Typography} from '@mui/material';
 import {getTutorials} from '@/app/[locale]/tutorials/util';
+import {LinkButton} from '@/components/ListItemLink';
+import {InfoOutlined} from '@mui/icons-material';
 
 export default async function Page() {
   const tutorials = getTutorials();
@@ -7,14 +10,18 @@ export default async function Page() {
   return (
     <>
       <Paper sx={{p: 2}}>
-        <h1>Tutorials</h1>
-        <ul>
+        <Typography variant="h1">Tutorials</Typography>
+        <List>
           {tutorials.map(tutorial => (
-            <li key={tutorial.slug}>
-              <a href={`/tutorials/${tutorial.slug}`}>{tutorial.slug}</a>
-            </li>
+            <LinkButton
+              key={tutorial.slug}
+              href={`tutorials/${tutorial.slug}`}
+              text={tutorial.slug}
+            >
+              <InfoOutlined />
+            </LinkButton>
           ))}
-        </ul>
+        </List>
       </Paper>
     </>
   );
